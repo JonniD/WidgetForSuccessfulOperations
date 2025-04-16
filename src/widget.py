41d6dfb2fc913f823cafeea13293297408d47092
Евгений Деревщюков <eugeny.derevshyuckoff@gmail.com>
2025-04-16 12:45:45 +0400

    complitecompleted code

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,11 @@
-from masks import get_mask_account
-from masks import get_mask_card_number
+from masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(account_card: str) -> str:
+    """Функция, которая умеет обрабатывать информацию как о картах и счетах."""
     account_card_list = account_card.split()
-    if account_card_list[0] == 'Счет':
-        return f'Счет {get_mask_account(account_card_list[1])}'
+    if account_card_list[0] == "Счет":
+        return f"Счет {get_mask_account(account_card_list[1])}"
     else:
         mask_card = []
         for element in account_card_list:
@@ -14,10 +15,12 @@ def mask_account_card(account_card: str) -> str:
                 mask_card.append(get_mask_card_number(element))
         return " ".join(mask_card)
 
-def get_date(date_and_time:str) -> str:
-    date_list = date_and_time.split('-')
-    date = f'{date_list[2][:2]}.{date_list[1]}.{date_list[0]}'
+
+def get_date(date_and_time: str) -> str:
+    """
+    Функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ"
+    """
+    date_list = date_and_time.split("-")
+    date = f"{date_list[2][:2]}.{date_list[1]}.{date_list[0]}"
     return date
-
-
-
