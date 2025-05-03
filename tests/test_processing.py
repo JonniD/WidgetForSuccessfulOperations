aaceaa9de7ee6017b75213a3a74859c89cbdc3b4
Евгений Деревщюков <eugeny.derevshyuckoff@gmail.com>
2025-05-04 00:32:43 +0400

    added a module processing.py and README.md

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -2,8 +2,8 @@ from src.processing import filter_by_state, sort_by_date
 
 
 def test_filter_by_state(list_of_dic, list_of_EXECUTED, list_of_CANCELED):
-    assert filter_by_state(list_of_dic, 'EXECUTED') == list_of_EXECUTED
-    assert filter_by_state(list_of_dic, 'CANCELED') == list_of_CANCELED
+    assert filter_by_state(list_of_dic, "EXECUTED") == list_of_EXECUTED
+    assert filter_by_state(list_of_dic, "CANCELED") == list_of_CANCELED
     assert filter_by_state(list_of_dic) == list_of_EXECUTED
 
 
