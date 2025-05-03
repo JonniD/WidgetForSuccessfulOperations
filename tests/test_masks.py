24d0e45340659546da8ca22e01387a60881d74c1
Евгений Деревщюков <eugeny.derevshyuckoff@gmail.com>
2025-05-03 20:50:36 +0400

    added a module processing.py and README.md

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -8,7 +8,6 @@ def test_get_mask_card_number(card_number1, card_number2, card_number3, number, 
     assert get_mask_card_number(number) == None
     assert get_mask_card_number(empty_string) == None
 
-
 def test_get_mask_account(account1, account2, account3, number, empty_string):
     assert get_mask_account(account1) == "**4305"
     assert get_mask_account(account2) == "**4305"
