e17f85d89a38610d26e1ce01062391616a08093c
Евгений Деревщюков <eugeny.derevshyuckoff@gmail.com>
2025-05-11 02:10:20 +0400

    added a module processing.py and README.md

---
+++ src/generators.py
@@ -0,0 +1,22 @@
+from mypyc.primitives.misc_ops import yield_from_except_op
+
+
+def filter_by_currency(all_transactions, name_currency: str):
+    for transaction in all_transactions:
+        if transaction["operationAmount"]["currency"]["name"] == name_currency:
+            yield transaction
+
+def transaction_descriptions(transactions):
+    for transaction in transactions:
+            yield transaction["description"]
+
+
+def card_number_generator(start,end):
+    for num in range(start, end+1):
+        str_num = f"{num:016}"
+        yield f'{str_num[:4]} {str_num[4:8]} {str_num[8:12]} {str_num[12:]}'
+
+
+for card_number in card_number_generator(5000000000000, 5000000000011):
+    print(card_number)
+
