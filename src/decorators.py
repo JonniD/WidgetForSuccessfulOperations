4914c713f217e5792a4fae29a636c6f89e0207bc
Евгений Деревщюков <eugeny.derevshyuckoff@gmail.com>
2025-05-15 12:57:34 +0400

    created decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,27 @@
+from functools import wraps
+
+
+def log(filename=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                message = f"{func.__name__} ok"
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(message + "\n")
+                else:
+                    print(message)
+                return result
+            except Exception as e:
+                message = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
+                if filename:
+                    with open(filename, "a") as file:
+                        file.write(message + "\n")
+                else:
+                    print(message)
+                raise
+        return wrapper
+    return decorator
+
