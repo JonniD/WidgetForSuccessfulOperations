09d547d96bbcf1fbe6cc9637fad56f39950e5f45
Евгений Деревщюков <eugeny.derevshyuckoff@gmail.com>
2025-05-16 21:19:56 +0400

    created test_decorators.py

---
+++ tests/test_decorators.py
@@ -0,0 +1,43 @@
+import pytest
+from src.decorators import log
+import tempfile
+import os
+
+
+def test_log_success(capsys):
+
+    @log()
+    def my_function(x, y):
+        return x + y
+
+    my_function(1, 2)
+    captured = capsys.readouterr()
+    assert captured.out == 'my_function ok\n'
+
+
+def test_log_to_file():
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        filename = tmp.name
+
+    @log(filename=filename)
+    def test_func(x):
+        return x
+
+    test_func(1)
+
+    with open(filename) as f:
+        content = f.read()
+
+    os.unlink(filename)
+    assert "test_func ok" in content
+
+
+
+def test_log_error(capsys):
+    @log()
+    def test_func(a, b):
+        raise ValueError('test error')
+
+    test_func(1, 2)
+    captured = capsys.readouterr()
+    assert 'test_func error: ValueError. Inputs: (1, 2), {}\n' in captured.out
